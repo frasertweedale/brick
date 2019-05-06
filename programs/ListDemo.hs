933dee647065549344ee90a0cdcfab00572400a0
Fraser Tweedale <frase@frase.id.au>
2019-05-06 17:40:51 +1000

    infinite scroll demo

--- programs/ListDemo.hs
+++ programs/ListDemo.hs
@@ -1,22 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE CPP #-}
 module Main where
 
-import Lens.Micro ((^.))
+import Lens.Micro ((^.), over, both, set)
 import Control.Monad (void)
 #if !(MIN_VERSION_base(4,11,0))
 import Data.Monoid
 #endif
-import Data.Maybe (fromMaybe)
+import qualified Data.List
 import qualified Graphics.Vty as V
 
 import qualified Brick.Main as M
+import Brick.BChan (BChan, newBChan, writeBChan)
 import qualified Brick.Types as T
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.List as L
 import qualified Brick.Widgets.Center as C
 import qualified Brick.AttrMap as A
-import qualified Data.Vector as Vec
 import Brick.Types
   ( Widget
   )
@@ -30,54 +33,59 @@ import Brick.Widgets.Core
   )
 import Brick.Util (fg, on)
 
-drawUI :: (Show a) => L.List () a -> [Widget ()]
-drawUI l = [ui]
+newtype L a = L [a]
+  deriving (Functor, Foldable, Traversable)
+
+instance L.Splittable L where
+  splitAt i (L xs) = over both L (Data.List.splitAt i xs)
+
+drawUI :: (Show a) => (chan, L.GenericList () L a) -> [Widget ()]
+drawUI (_, l) = [ui]
     where
         label = str "Item " <+> cur <+> str " of " <+> total
-        cur = case l^.(L.listSelectedL) of
+        cur = case l^.L.listSelectedL of
                 Nothing -> str "-"
                 Just i -> str (show (i + 1))
-        total = str $ show $ Vec.length $ l^.(L.listElementsL)
+        total = str "?"
         box = B.borderWithLabel label $
-              hLimit 25 $
-              vLimit 15 $
               L.renderList listDrawElement True l
         ui = C.vCenter $ vBox [ C.hCenter box
                               , str " "
-                              , C.hCenter $ str "Press +/- to add/remove list elements."
                               , C.hCenter $ str "Press Esc to exit."
                               ]
 
-appEvent :: L.List () Char -> T.BrickEvent () e -> T.EventM () (T.Next (L.List () Char))
-appEvent l (T.VtyEvent e) =
-    case e of
-        V.EvKey (V.KChar '+') [] ->
-            let el = nextElement (L.listElements l)
-                pos = Vec.length $ l^.(L.listElementsL)
-            in M.continue $ L.listInsert pos el l
+appEvent
+  :: (BChan (), L.GenericList () L Int)
+  -> T.BrickEvent () e
+  -> T.EventM () (T.Next (BChan (), L.GenericList () L Int))
+appEvent s@(chan, l) ev = case ev of
+  T.VtyEvent (V.EvKey V.KEsc []) -> M.halt s
+  T.VtyEvent vtyEv -> M.continue . (chan,) . pruneList =<< L.handleListEvent vtyEv l
+  _ -> M.continue s
 
-        V.EvKey (V.KChar '-') [] ->
-            case l^.(L.listSelectedL) of
-                Nothing -> M.continue l
-                Just i -> M.continue $ L.listRemove i l
-
-        V.EvKey V.KEsc [] -> M.halt l
-
-        ev -> M.continue =<< L.handleListEvent ev l
-    where
-      nextElement :: Vec.Vector Char -> Char
-      nextElement v = fromMaybe '?' $ Vec.find (flip Vec.notElem v) (Vec.fromList ['a' .. 'z'])
-appEvent l _ = M.continue l
+pruneList
+  :: (L.Splittable t)
+  => L.GenericList n t a -> L.GenericList n t a
+pruneList l =
+  case l ^. L.listSelectedL of
+    Nothing -> l
+    Just i ->
+      let
+        i' = max 0 (i - 999)
+      in
+        ($l) $
+          over L.listElementsL (snd . L.splitAt i')
+          . set L.listSelectedL (Just (i - i'))
 
 listDrawElement :: (Show a) => Bool -> a -> Widget ()
 listDrawElement sel a =
     let selStr s = if sel
                    then withAttr customAttr (str $ "<" <> s <> ">")
                    else str s
-    in C.hCenter $ str "Item " <+> (selStr $ show a)
+    in C.hCenter $ str "Item " <+> selStr (show a)
 
-initialState :: L.List () Char
-initialState = L.list () (Vec.fromList ['a','b','c']) 1
+initialState :: L.GenericList () L Int
+initialState = L.list () (L (take 10000000 [0..])) 1
 
 customAttr :: A.AttrName
 customAttr = L.listSelectedAttr <> "custom"
@@ -89,7 +97,7 @@ theMap = A.attrMap V.defAttr
     , (customAttr,            fg V.cyan)
     ]
 
-theApp :: M.App (L.List () Char) e ()
+theApp :: M.App (BChan (), L.GenericList () L Int) e ()
 theApp =
     M.App { M.appDraw = drawUI
           , M.appChooseCursor = M.showFirstCursor
@@ -99,4 +107,8 @@ theApp =
           }
 
 main :: IO ()
-main = void $ M.defaultMain theApp initialState
+main = do
+  chan <- newBChan 32
+  let buildVty = V.mkVty V.defaultConfig
+  initialVty <- buildVty
+  void $ M.customMain initialVty buildVty (Just chan) theApp (chan, initialState)
